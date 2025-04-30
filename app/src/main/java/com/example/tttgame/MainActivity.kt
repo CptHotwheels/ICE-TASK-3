02ed240221f41f4b8b6f9f17ad20cecd8b733e86
ST10467763 IMAD A1 <st10467763@vcconnect.edu.com>
2025-04-30 13:08:20 +0200

    ICE Task 3 commit.

---
+++ app/src/main/java/com/example/tttgame/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.tttgame
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tttgame.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity()
+{
+    enum class Turn
+    {
+      NOUGHT,
+      CROSS
+    }
+
+    private var firstTurn = Turn.CROSS
+    private var currentTurn = Turn.CROSS
+
+    private var boardList = mutableListOf<Button>()
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initBoard()
+    }
+
+    private fun initBoard()
+    {
+        boardList.add(binding.a1)
+        boardList.add(binding.a2)
+        boardList.add(binding.a3)
+        boardList.add(binding.b1)
+        boardList.add(binding.b2)
+        boardList.add(binding.b3)
+        boardList.add(binding.c1)
+        boardList.add(binding.c2)
+        boardList.add(binding.c3)
+    }
+
+    fun boardTapped(view: View)
+    {
+        if(view !is Button)
+            return
+        addToButton(view)
+    }
+
+    private fun addToButton(button: Button)
+    {
+        if(button.text != "")
+            return
+
+        if(currentTurn == Turn.NOUGHT)
+        {
+            button.text = NOUGHT
+            currentTurn = Turn.CROSS
+        }
+        else if(currentTurn == Turn.CROSS)
+        {
+            button.text = CROSS
+            currentTurn = Turn.NOUGHT
+        }
+        setTurnLabel()
+    }
+
+    private fun setTurnLabel()
+    {
+        var turnText = ""
+        if(currentTurn == Turn.CROSS)
+            turnText = "Turn $CROSS"
+        if(currentTurn == Turn.NOUGHT)
+            turnText = "Turn $NOUGHT"
+
+        binding.turnTextView.text = turnText
+    }
+
+    companion object
+    {
+        const val NOUGHT = "O"
+        const val CROSS = "X"
+    }
+}
